a32f46e152b44c3a5b5608b36de91cad596b064d
Yash Gediya <yash.gediya@bytestechnolab.in>
2022-04-28 18:03:22 +0530

    changes

--- src/pages/index.js
+++ src/pages/index.js
@@ -25,7 +25,7 @@ const IndexPage = () => (
       <Link to="/using-ssr">Go to "Using SSR"</Link> <br />
       <Link to="/using-dsg">Go to "Using DSG"</Link>
     </p>
-    <button>Click this button</button>
+    <button className="call-to-action">Click this button</button>
   </Layout>
 )
 
