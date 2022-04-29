0ee343040f20aab49ce070c6c7dd79df62bd41f2
Yash Gediya <yash.gediya@bytestechnolab.in>
2022-04-29 13:39:32 +0530

    changes

--- src/pages/index.js
+++ src/pages/index.js
@@ -4,43 +4,48 @@ import { StaticImage } from "gatsby-plugin-image"
 
 import Layout from "../components/layout"
 import Seo from "../components/seo"
+import axios from "axios"
 
-const IndexPage = () => (
-  <Layout>
-    <Seo title="Home" />
-    <h1>Hi people</h1>
-    <p>Welcome to your new Gatsby site.</p>
-    <p>Now go build something great.</p>
-    <StaticImage
-      src="../images/gatsby-astronaut.png"
-      width={300}
-      quality={95}
-      formats={["auto", "webp", "avif"]}
-      alt="A Gatsby astronaut"
-      style={{ marginBottom: `1.45rem` }}
-    />
-    <p>
-      <Link to="/page-2/">Go to page 2</Link> <br />
-      <Link to="/using-typescript/">Go to "Using TypeScript"</Link> <br />
-      <Link to="/using-ssr">Go to "Using SSR"</Link> <br />
-      <Link to="/using-dsg">Go to "Using DSG"</Link>
-    </p>
-    <button
-      className="call-to-action"
-      onClick={e => {
-        e.preventDefault()
-        window.dataLayer = window.dataLayer || []
-        window.dataLayer.push({
-          event: "buttonClick",
-          category: "Custom Button",
-          action: "click",
-          lable: "Google Analytics Custom Button",
-        })
-      }}
-    >
-      Click this button
-    </button>
-  </Layout>
-)
+const IndexPage = () => {
+  axios.get("https://jsonplaceholder.typicode.com/users").then(response => {
+    console.log(response.data)
+  })
+
+  const handleClickEvent = e => {
+    e.preventDefault()
+    window.dataLayer = window.dataLayer || []
+    window.dataLayer.push({
+      event: "buttonClick",
+      category: "Custom Button",
+      action: "click",
+      lable: "Google Analytics Custom Button",
+    })
+  }
+  return (
+    <Layout>
+      <Seo title="Home" />
+      <h1>Hi people</h1>
+      <p>Welcome to your new Gatsby site.</p>
+      <p>Now go build something great.</p>
+      <StaticImage
+        src="../images/gatsby-astronaut.png"
+        width={300}
+        quality={95}
+        formats={["auto", "webp", "avif"]}
+        alt="A Gatsby astronaut"
+        style={{ marginBottom: `1.45rem` }}
+      />
+      <p>
+        <Link to="/page-2/">Go to page 2</Link> <br />
+        <Link to="/using-typescript/">Go to "Using TypeScript"</Link> <br />
+        <Link to="/using-ssr">Go to "Using SSR"</Link> <br />
+        <Link to="/using-dsg">Go to "Using DSG"</Link>
+      </p>
+      <button className="call-to-action" onClick={handleClickEvent}>
+        Click this button
+      </button>
+    </Layout>
+  )
+}
 
 export default IndexPage
